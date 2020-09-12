46ae6f620e957538018075d490f9c59197c0803f
numabesachi <numabesachi@gmail.com>
2020-09-12 13:57:47 +0900

    finish

---
+++ app/src/main/java/com/example/l4s_first/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.l4s_first
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            button.text = "クリックされました!"
+
+        }
+    }
+
+}
